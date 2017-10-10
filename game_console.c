19a355be72a4728f6255fc82529e25a696795b03
Shiyu Zhang <steve@biofuelinnovations.com.au>
2017-10-11 00:38:02 +1100

    fucking UP button burn my shit up

--- game_console.c
+++ game_console.c
@@ -14,42 +14,90 @@ DESCRIPTION:
 #include "game_console.h" 
 
 
-int main(void)
-{
-	/*pin init*/
+void button_init(void){
 	
-	//BAT_LOW_LED(OFF); //Make sure it is off before changing direction
-	//BAT_LOW_LED_DIR(OUT); //Set BATTERY LED I/Os as outputs.
-	//UP_BUTTON_DIR(IN); //Set UP_BUTTON I/Os as input.
-
-
+	UP_BUTTON_DIR(IN); 
+	UP_BUTTON_PULL(ON);  
 	
-	//try to turn on LCD backlight
-	LCD_BACKLIGHT_DIR(OUT);
-	LCD_BACKLIGHT(OFF);
+	DOWN_BUTTON_DIR(IN); 
+	DOWN_BUTTON_PULL(ON); 
 
-	BAT_LOW_LED_DIR(OUT);
-	BAT_LOW_LED(ON);
+	LEFT_BUTTON_DIR(IN); 
+	LEFT_BUTTON_PULL(ON);
+
+	RIGHT_BUTTON_DIR(IN); 
+	RIGHT_BUTTON_PULL(ON);
+
+	A_BUTTON_DIR(IN);
+	A_BUTTON_PULL(ON);
 	
-	while (TRUE)//Master loop always true so always loop
-	{
+	B_BUTTON_DIR(IN);
+	B_BUTTON_PULL(ON);
 
-		//TRY TO DETECT BUTTON BY INTERUPT???
+}
 
-
-
-		//turn everything off
-		
-		/*//Turn on the LED if UP_BUTTON is pressed
-		if (UP_BUTTON)
+void button_det(void){
+					//Turn on the LED if UP_BUTTON is pressed
+					//Turn on the LED if UP_BUTTON is pressed
+		if (UP_BUTTON)//get device status
 		{
 			BAT_LOW_LED(ON);	
 		} 	
 		else
 		{
 			BAT_LOW_LED(OFF);
-		}*/
+		}
+					//Turn on the LED if UP_BUTTON is pressed
+		if (DOWN_BUTTON)//get device status
+		{
+			BAT_LOW_LED(ON);	
+		} 	
+		else
+		{
+			BAT_LOW_LED(OFF);
+		}
+
+		if (A_BUTTON)//get device status
+		{
+			BAT_LOW_LED(ON);	
+		} 	
+		else
+		{
+			BAT_LOW_LED(OFF);
+		}
+		
+		
+		//Turn on the LED if UP_BUTTON is pressed
+		if (B_BUTTON)//get device status
+		{
+			BAT_LOW_LED(ON);	
+		} 	
+		else
+		{
+			BAT_LOW_LED(OFF);
+		}
+
+}
+
+int main(void)
+{
+	/*pin init*/
+	button_init();//initialise the buttons and pull up
+
+	//try to turn on LCD backlight
+	LCD_BACKLIGHT_DIR(OUT);
+	LCD_BACKLIGHT(OFF);
+
+	BAT_LOW_LED_DIR(OUT);
+	BAT_LOW_LED(OFF);
+	
+	while (TRUE)//Master loop always true so always loop
+	{
+		button_det();
+	
 
 	}
 
+
+	
 }
